715ba6165f35ede1359afa11e2110115f7a3e135
John Fletcher <johnfletcherdev@gmail.com>
2025-07-05 15:28:42 -0600

    added correct formatting

--- src/main/java/com/jonathanfletcher/worldstage_api/model/entity/User.java
+++ src/main/java/com/jonathanfletcher/worldstage_api/model/entity/User.java
@@ -64,7 +64,7 @@ public class User implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName().toString()))
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().toString()))
                 .collect(Collectors.toList());
     }
 
